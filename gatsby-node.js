a7076a666da7ad81f456b95d5e7ba6206bbaf7bc
Imanishi Toshifumi <toshifumi.imanishi@un-t.com>
2019-11-16 18:16:19 +0900

    リファクタリング

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,30 @@
 const path = require(`path`)
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  return new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-
-    resolve(
-      graphql(`
-        {
-          allContentfulCourse {
-            edges {
-              node {
-                title
-                slug
-              }
-            }
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const sources = await graphql(`
+    {
+      allContentfulCourse {
+        edges {
+          node {
+            title
+            slug
           }
         }
-      `).then(result => {
-        if (result.errors) {
-          reject(result.errors)
-        }
+      }
+    }
+  `)
 
-        result.data.allContentfulCourse.edges.forEach(edge => {
-          createPage({
-            path: `${edge.node.slug}`,
-            component: blogPostTemplate,
-            context: {
-              slug: edge.node.slug
-            }
-          })
-        })
-      })
-    )
+  if (sources.errors) return
+
+  const posts = sources.data.allContentfulCourse.edges
+
+  posts.forEach(({ node }) => {
+    createPage({
+      path: `${node.slug}`,
+      component: path.resolve(`src/templates/blog-post.js`),
+      context: {
+        slug: node.slug
+      }
+    })
   })
 }
